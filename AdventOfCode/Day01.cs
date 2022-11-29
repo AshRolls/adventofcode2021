55d4add570ed51d32b114e6c4a77a7272c6c66be
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-11-29 10:37:17 +0000

    solve first part (correct 1791)

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,30 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+
+        int last;
+        Int32.TryParse(_input[0], out last);
+
+        int count = 0;
+        for (int i=1; i<_input.Length;i++)
+        {
+            int current;
+            Int32.TryParse(_input[i], out current);
+            if (current > last) count++;
+            Int32.TryParse(_input[i], out last);
+        }
+
+        return new(count.ToString());
+    }
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
