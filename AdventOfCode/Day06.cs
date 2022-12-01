42aa7a491794db218dc27342608efe199e272b6b
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 11:11:28 +0000

    sovle part two (1632779838045)

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -24,7 +24,38 @@ public class Day06 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("Not Solved");
+        string[] strs = _input[0].Split(',');
+        List<short> shoal = Array.ConvertAll(strs, s => short.Parse(s)).ToList();
+        long[] ages = new long[9];
+
+        initAges(ages, shoal);
+
+        for (int i = 0; i < 256; i++)
+        {
+            doDayOpt(ages);
+        }
+
+        return new(ages.Sum().ToString());
+    }
+
+    private void initAges(long[] ages, List<short> shoal)
+    {
+        foreach(short s in shoal)
+        {
+            ages[s]++;
+        }
+    }
+
+    private void doDayOpt(long[] ages)
+    {
+        long breeders = ages[0];
+        ages[0] = 0;
+        for (int i = 1; i < 9; i++)
+        {
+            ages[i - 1] = ages[i];
+        }
+        ages[6] += breeders;
+        ages[8] = breeders;
     }
 
     private void doDay(List<short> shoal)
