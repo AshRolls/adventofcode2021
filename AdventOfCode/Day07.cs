8da8eab771a737dda81dca9be78bc4097a660839
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 12:31:58 +0000

    solve part two (97164301)

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -13,14 +13,11 @@ public class Day07 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         string[] strs = _input[0].Split(',');
-        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));        
-        int rndAvg = (int)Math.Round(positions.Average());
+        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));                
         
         int min = positions.Min();
         int max = positions.Max();
         int mid = ((max - min) / 2) + min;
-        //int fuelMin = checkFuelUse(positions, min);
-        //int fuelMax = checkFuelUse(positions, max);
         
         int bestF = checkFuelUse(positions, mid);
         int nextF = checkFuelUse(positions, mid + 1);
@@ -50,23 +47,94 @@ public class Day07 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("Not Solved");
+        string[] strs = _input[0].Split(',');
+        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));
+
+        int min = positions.Min();
+        int max = positions.Max();
+        int mid = ((max - min) / 2) + min;
+
+        double bestF = checkFuelUse2(positions, mid);
+        double nextF = checkFuelUse2(positions, mid + 1);
+        int best = mid;
+        int next = mid + 1;
+        bool reversed = false;
+
+        if (nextF > bestF)
+        {
+            reversed = true;
+            next = mid - 1;
+            nextF = checkFuelUse2(positions, next);
+        }
+
+        // carry on checking next until it stops dropping
+        while (nextF < bestF)
+        {
+            best = next;
+            bestF = nextF;
+            if (!reversed) next++;
+            else next--;
+            nextF = checkFuelUse2(positions, next);
+        }
+
+        return new(bestF.ToString());
     }
 
-    private int checkFuelUse(int[] positions, int mid)
+    private int checkFuelUse(int[] positions, int h)
     {
         int fuel = 0;
 
         for (int i = 0; i< positions.Length; i++)
         {
-            if (positions[i] < mid)
+            if (positions[i] < h)
             {
-                fuel += mid - positions[i];
+                fuel += h - positions[i];
             }
             else
             {
-                fuel += positions[i] - mid;
+                fuel += positions[i] - h;
             }
+        }
+
+        return fuel;
+    }
+
+    private double checkFuelUse2(int[] positions, int h)
+    {
+        double fuel = 0;
+        int steps;
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            if (positions[i] < h)
+            {
+                steps = h - positions[i];
+                fuel = calcFuel(fuel, steps);
+            }
+            else
+            {
+                steps = positions[i] - h;
+                fuel = calcFuel(fuel, steps);
+            }
+        }
+
+        return fuel;
+    }
+
+    private static double calcFuel(double fuel, int steps)
+    {
+        if (steps > 0)
+        {
+            int pair = steps + 1;
+            double t;
+            if (steps % 2 == 0) t = pair * (steps / 2);
+            else
+            {
+                t = pair * ((steps + 1) / 2);
+                t -= pair / 2;
+            }
+
+            fuel += t;
         }
 
         return fuel;
