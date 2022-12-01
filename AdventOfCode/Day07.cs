050dbf020a41dff77263de7a0deaf3c4fcc5c8ba
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 12:06:43 +0000

    solve first part (344297)

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -13,15 +13,62 @@ public class Day07 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         string[] strs = _input[0].Split(',');
-        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));
-        double avg = positions.Average();
-        int rndAvg = (int)Math.Round(avg);
+        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));        
+        int rndAvg = (int)Math.Round(positions.Average());
+        
+        int min = positions.Min();
+        int max = positions.Max();
+        int mid = ((max - min) / 2) + min;
+        //int fuelMin = checkFuelUse(positions, min);
+        //int fuelMax = checkFuelUse(positions, max);
+        
+        int bestF = checkFuelUse(positions, mid);
+        int nextF = checkFuelUse(positions, mid + 1);
+        int best = mid;
+        int next = mid + 1;
+        bool reversed = false;
 
-        return new(rndAvg.ToString());
+        if (nextF > bestF)
+        {          
+            reversed = true;                      
+            next = mid - 1;
+            nextF = checkFuelUse(positions, next);
+        }        
+
+        // carry on checking next until it stops dropping
+        while (nextF < bestF)
+        {
+            best = next;
+            bestF = nextF;
+            if (!reversed) next++;
+            else next--;
+            nextF = checkFuelUse(positions, next);
+        }
+
+        return new(bestF.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
         return new("Not Solved");
+    }
+
+    private int checkFuelUse(int[] positions, int mid)
+    {
+        int fuel = 0;
+
+        for (int i = 0; i< positions.Length; i++)
+        {
+            if (positions[i] < mid)
+            {
+                fuel += mid - positions[i];
+            }
+            else
+            {
+                fuel += positions[i] - mid;
+            }
+        }
+
+        return fuel;
     }
 }
