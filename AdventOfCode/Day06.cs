9a5165cdfd856c9aaeb4dea72c5910d832dca3fa
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 10:49:06 +0000

    solve first part (360761)

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private readonly string[] _input;    
+
+    public Day06()
+    {
+        _input = File.ReadAllLines(InputFilePath);        
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        string[] strs = _input[0].Split(',');
+        List<short> shoal = Array.ConvertAll(strs, s => short.Parse(s)).ToList();
+
+        for (int i = 0; i < 80; i++)
+        {
+            doDay(shoal);
+        }
+
+        return new(shoal.Count().ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("Not Solved");
+    }
+
+    private void doDay(List<short> shoal)
+    {
+        int toAdd = 0;
+
+        for (int i = 0; i < shoal.Count; i++)
+        {
+            if (shoal[i] == 0) //  breed
+            {
+                shoal[i] = 6;
+                toAdd++;
+            }
+            else
+            {
+                shoal[i]--;
+            }
+        }
+
+        for (int i = 0; i < toAdd; i++)
+        {
+            shoal.Add(8);
+        }
+    }
+
+}
