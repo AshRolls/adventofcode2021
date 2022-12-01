aa945cc8949607ded3dc540190aea3b9629fc7b3
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 11:27:57 +0000

    Inputs and first effort to use an average (incorrect)

---
+++ AdventOfCode/Day07.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode;
+
+public class Day07 : BaseDay
+{
+    private readonly string[] _input;    
+
+    public Day07()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        string[] strs = _input[0].Split(',');
+        int[] positions = Array.ConvertAll(strs, s => int.Parse(s));
+        double avg = positions.Average();
+        int rndAvg = (int)Math.Round(avg);
+
+        return new(rndAvg.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("Not Solved");
+    }
+}
