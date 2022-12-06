5255e1f2db8687c06374bcaae038fbc983d37ddb
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-06 11:41:46 +0000

    solve second part (3049320156)

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -15,7 +15,11 @@ public class Day10 : BaseDay
         { '}', 1197 },
         { '>', 25137 } };
 
-
+    private static readonly Dictionary<char, int> _scores2 = new Dictionary<char, int>() {
+        { '(', 1 },
+        { '[', 2 },
+        { '{', 3 },
+        { '<', 4 } };
 
     public Day10()
     {
@@ -25,34 +29,60 @@ public class Day10 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         int p = 0;
-        foreach(string s in _input)
+        foreach (string s in _input)
             p += openChunk(0, 0, new Stack<char>(), s);
-        //p += openChunk(0, 0, new Stack<char>(), _input[0]);
 
         return new(p.ToString());
-    }    
+    }
 
     public override ValueTask<string> Solve_2()
     {
-        return new("Not Solved");
+        List<string> incomplete = new List<string>();
+        Stack<char> openC = new Stack<char>();
+        foreach (string s in _input)
+        {
+            if (openChunk(0, 0, new Stack<char>(), s) == 0) incomplete.Add(s);
+        }
+
+        List<long> scores = new List<long>();
+        foreach (string s in incomplete)
+        {
+            openC = openChunkStack(0, 0, new Stack<char>(), s);
+            scores.Add(calcScore(openC));
+        }
+        scores.Sort();
+        long score = scores[scores.Count / 2];
+
+        return new(score.ToString());
+    }
+
+    private long calcScore(Stack<char> openC)
+    {
+        long score = 0;
+        while (openC.Any())
+        {
+            score *= 5;
+            score += _scores2[openC.Pop()];
+        }
+        return score;
     }
 
     private static int openChunk(int p, int i, Stack<char> openC, string line)
     {
         int points = p;
-        char c = line[i];       
+        char c = line[i];
         if (_pairs.ContainsKey(c))
         {
             //Console.Out.WriteLine("Open " + c);
             openC.Push(c);
-            if (i + 1 < line.Length) points += openChunk(p, i + 1, openC, line);            
+            if (i + 1 < line.Length) points += openChunk(p, i + 1, openC, line);
         }
         else if (_pairs[openC.Peek()] != c)
         {
             //Console.Out.WriteLine("Fault " + c);
-            openC.Pop();            
-            points += _scores[c];            
-        }        
+            openC.Pop();
+            points += _scores[c];
+        }
         else if (i + 1 < line.Length)
         {
             //Console.Out.WriteLine("Close " + c);
@@ -60,5 +90,23 @@ public class Day10 : BaseDay
             points += openChunk(p, i + 1, openC, line);
         }
         return points;
+    }
+
+    private static Stack<char> openChunkStack(int p, int i, Stack<char> openC, string line)
+    {
+        char c = line[i];
+        if (_pairs.ContainsKey(c))
+        {
+            //Console.Out.WriteLine("Open " + c);
+            openC.Push(c);
+            if (i < line.Length - 1) return openChunkStack(p, i + 1, openC, line);            
+        }
+        else 
+        {
+            //Console.Out.WriteLine("Close " + c);
+            openC.Pop();
+            if (i < line.Length - 1) return openChunkStack(p, i + 1, openC, line);
+        }
+        return openC;
     }
 }
