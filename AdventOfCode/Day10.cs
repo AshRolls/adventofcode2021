3ec9a3d748e7dd9863ff7a9105249dd356a5dc32
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-06 11:06:37 +0000

    solve first part (339477)

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -2,20 +2,63 @@
 
 public class Day10 : BaseDay
 {
-    private readonly string[] _input;    
+    private readonly string[] _input;
+    private static readonly Dictionary<char, char> _pairs = new Dictionary<char, char>() {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' } };
+
+    private static readonly Dictionary<char, int> _scores = new Dictionary<char, int>() {
+        { ')', 3 },
+        { ']', 57 },
+        { '}', 1197 },
+        { '>', 25137 } };
+
+
 
     public Day10()
     {
-        _input = File.ReadAllLines(InputFilePath);        
+        _input = File.ReadAllLines(InputFilePath);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        return new("Not Solved");
-    }
+        int p = 0;
+        foreach(string s in _input)
+            p += openChunk(0, 0, new Stack<char>(), s);
+        //p += openChunk(0, 0, new Stack<char>(), _input[0]);
+
+        return new(p.ToString());
+    }    
 
     public override ValueTask<string> Solve_2()
     {
         return new("Not Solved");
+    }
+
+    private static int openChunk(int p, int i, Stack<char> openC, string line)
+    {
+        int points = p;
+        char c = line[i];       
+        if (_pairs.ContainsKey(c))
+        {
+            //Console.Out.WriteLine("Open " + c);
+            openC.Push(c);
+            if (i + 1 < line.Length) points += openChunk(p, i + 1, openC, line);            
+        }
+        else if (_pairs[openC.Peek()] != c)
+        {
+            //Console.Out.WriteLine("Fault " + c);
+            openC.Pop();            
+            points += _scores[c];            
+        }        
+        else if (i + 1 < line.Length)
+        {
+            //Console.Out.WriteLine("Close " + c);
+            openC.Pop();
+            points += openChunk(p, i + 1, openC, line);
+        }
+        return points;
     }
 }
